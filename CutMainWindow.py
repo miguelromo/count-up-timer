c9484deeb558de62674c1295fba20132b4c76d09
Thilo Ratnaweera <thilo.ratnaweera@gmail.com>
2012-11-20 22:19:00 +0100

    Reorganized code.

--- CutMainWindow.py
+++ CutMainWindow.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python
  
 import sys
-from CutUtilQt import *
+import CutUtilQt
 from PyQt4 import QtGui
+from PyQt4 import QtCore
 
 class CutMainWindow(QtGui.QMainWindow):
 
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
 		self.setWindowTitle("Count Up Timer")
-		self.setCentralWidget(CutUtilQt.getContainerWidget())
+		self._timeDisplay = CutUtilQt.TimeDisplay()
+		self._controlPanel = CutUtilQt.ControlPanel()
+		self._centralWidget = QtGui.QWidget()
+		containerLayout = QtGui.QVBoxLayout()
+		containerLayout.addLayout(self._timeDisplay)
+		containerLayout.addLayout(self._controlPanel)
+		self._centralWidget.setLayout(containerLayout)
+		self.setCentralWidget(self._centralWidget)
+		#self._centralWidget = StaticMethods.getContainerWidget()
+		self.initializeSlots()
 		self.show()
+	
+	def initializeSlots(self):
+		QtCore.QObject.connect(self._controlPanel.getStartButton(), QtCore.SIGNAL("clicked()"), self.onStartButtonClicked)
+		QtCore.QObject.connect(self._controlPanel.getStopButton(), QtCore.SIGNAL("clicked()"), self.onStopButtonClicked)
+		QtCore.QObject.connect(self._controlPanel.getResetButton(), QtCore.SIGNAL("clicked()"), self.onResetButtonClicked)
+	
+	def onStartButtonClicked(self):
+		print "START"
 
+	def onStopButtonClicked(self):
+		print "STOP"
+
+	def onResetButtonClicked(self):
+		print "RESET"
 
 application = QtGui.QApplication(sys.argv)
 cutMainWindow = CutMainWindow()
--- CutUtilQt.py
+++ CutUtilQt.py
@@ -3,38 +3,62 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-class CutUtilQt:
+class TimeDisplay(QtGui.QHBoxLayout):
 
-	@staticmethod
-	def getContainerWidget():
-		containerWidget = QtGui.QWidget()
-		containerWidget.setLayout(CutUtilQt.getContainerLayout())
-		return containerWidget
+	def __init__(self):
+		super(TimeDisplay, self).__init__() # See [1]
+		self._lcd = QtGui.QLCDNumber()
+		self._lcd.setNumDigits(8)
+		self._defaultText = QtCore.QString('00:00:00')
+		self._lcd.display(QtCore.QString('00:04:36')) # Change to default text
+		self.addWidget(self._lcd)
+
+	def getLcd(self):
+		return self._lcd
 	
-	@staticmethod
-	def getContainerLayout():
-		containerLayout = QtGui.QVBoxLayout()
-		containerLayout.addLayout(CutUtilQt.getTimeDisplay())
-		containerLayout.addLayout(CutUtilQt.getControlWidgetsGroup())
-		return containerLayout
-	
-	@staticmethod
-	def getTimeDisplay():
-		containerLayout = QtGui.QHBoxLayout()
-		lcd = QtGui.QLCDNumber()
-		lcd.setNumDigits(8)
-		s = QtCore.QString('00:04:36')
-		lcd.display(s)
-		containerLayout.addWidget(lcd)
-		return containerLayout
+	def getDefaultText(self):
+		return self._defaultText
+
+
+class ControlPanel(QtGui.QHBoxLayout):
+
+	def __init__(self):
+		super(ControlPanel, self).__init__() # See [1]
+		self._startButton = QtGui.QPushButton("Start")
+		self._stopButton = QtGui.QPushButton("Stop")
+		self._resetButton = QtGui.QPushButton("Reset")
+		self.addWidget(self._startButton)
+		self.addWidget(self._stopButton)
+		self.addWidget(self._resetButton)
+
+	def getStartButton(self):
+		return self._startButton
+
+	def getStopButton(self):
+		return self._stopButton
+
+	def getResetButton(self):
+		return self._resetButton
+		
+		
+class StaticMethods:
 		
 	@staticmethod
 	def getControlWidgetsGroup():
 		containerLayout = QtGui.QHBoxLayout()
 		startButton = QtGui.QPushButton("Start")
+		QtCore.QObject.connect(startButton, QtCore.SIGNAL("clicked()"), StaticMethods.theShit)
 		stopButton = QtGui.QPushButton("Stop")
 		resetButton = QtGui.QPushButton("Reset")
 		containerLayout.addWidget(startButton)
 		containerLayout.addWidget(stopButton)
 		containerLayout.addWidget(resetButton)
 		return containerLayout
+	
+	@staticmethod
+	def theShit():
+		print "The Shit"
+
+
+# [1] http://stackoverflow.com/questions/6002895/understanding-the-underlying-c-c-object-has-been-deleted-error/10889343#10889343
+
