5400b7221e51312de3a722616eaeedd086d36626
Thilo Ratnaweera <thilo.ratnaweera@gmail.com>
2012-11-20 15:44:41 +0100

    Switched to Qt4.

--- CountUpTimer.py
+++ CountUpTimer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
+ 
+import sys
+from PyQt4 import QtGui
+ 
+class MainWindow(QtGui.QMainWindow):
+	def __init__(self):
+		QtGui.QMainWindow.__init__(self)
+		self.setWindowTitle("Count Up Timer")
+		self.resize(300, 300)
+		self.show()
 
-from Tkinter import *
-
-root = Tk()
-
-w = Label(root, text="Count Up Timer")
-w.pack()
-
-root.mainloop()
+app = QtGui.QApplication(sys.argv)
+f = MainWindow()
+app.exec_()
